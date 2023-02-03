70a8ec2a3487d74b77f8c1bbedf0d4644b3843da
Allan Ayford <60179207+allanayford-dev@users.noreply.github.com>
2023-02-03 04:22:42 +0200

    Service Registry Updated

---
+++ server/service-registry/server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const ServiceRegistry = require('./lib/ServiceRgistry');
+
+const service = express();
+
+module.exports = (config) => {
+	const log = config.log();
+	const serviceRegistry = new ServiceRegistry(log);
+
+	if (service.get('env') === 'development') {
+		service.use((req, res, next) => {
+			log.debug(`${req.method}: ${req.url}`);
+			return next();
+		});
+	}
+
+	service.put(
+		'/register/:servicename/:serviceversion/:serviceport',
+		(req, res) => {
+			const { servicename, serviceversion, serviceport } = req.params;
+
+			const serviceip = req.connection.remoteAddress.includes('::')
+				? `[${req.connection.remoteAddress}]`
+				: req.connection.remoteAddress;
+
+			const serviceKey = serviceRegistry.register(
+				servicename,
+				serviceversion,
+				serviceip,
+				serviceport
+			);
+
+			return res.json({ result: serviceKeygit });
+		}
+	);
+
+	service.delete(
+		'/register/:servicename/:serviceversion/:serviceport',
+		(req, res, next) => {
+			return next('Not Implemented');
+		}
+	);
+
+	service.get('/register/:servicename/:serviceversion', (req, res, next) => {
+		return next('Not Implemented');
+	});
+
+	service.use((error, req, res) => {
+		res.status(error.status || 500);
+		log.error(error);
+		return res.json({
+			error: {
+				message: error.message,
+			},
+		});
+	});
+
+	return service;
+};
